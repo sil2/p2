eb1041f943acb58e0dd56f4179850caadc08fa1b
Oleg Stremin <oleg@goli.com>
2022-09-05 09:36:47 -0400

    session

--- src/components/LanguageSelect.vue
+++ src/components/LanguageSelect.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <select class="
+  <select @change="setLocale" class="
       focus:outline-none 
       focus:ring
      focus:ring-red-500 
@@ -13,18 +13,28 @@
     focus:border-red-500
       sm:text-sm
       " 
-      v-model.lazy="$i18n.locale">
-    <option :selected="[$i18n.locale ? 'selected' : 'f']" v-for="locale in $i18n.availableLocales" :key="locale"
+      v-model.lazy="$i18n.locale"
+  >
+    <option :selected="[$i18n.locale ? 'selected' : '']" v-for="locale in $i18n.availableLocales" :key="locale"
       :value="locale">{{locale}}</option>
   </select>
-
+{{storage.getStorageSync('locale')}}
 </template>
   
   <script setup>
   import { ref } from 'vue'
 
+  import { useStorage } from "vue3-storage";
 
-  const selected = ref('en')
 
+    const storage = useStorage();
+    const selected =  ref('en') //storage.hasKey('locale')? storage.getStorageSync('locale'):ref('en')
+
+    function setLocale(){
+      console.log('lalal',selected)
+      storage.setStorageSync("locale", selected.value);
+    }
+
+   
 
   </script>
