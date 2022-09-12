d91c7019fd558a41e121d89239982f5b6b75a7b8
Andrei <andrei@goli.com>
2022-09-12 16:44:57 -0400

    login page improvements

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,94 +1,106 @@
 <template>
-
-  <nav class="
-        px-6
-        py-2
-        mx-auto
-
-        md:flex md:justify-between md:items-center
-      ">
+  <nav class="px-6 py-2 mx-auto md:flex md:justify-between md:items-center">
     <div class="flex items-center justify-between">
       <router-link to="/">
-        <LogoSVG class="h-18 w-auto text-center text-white	 fill-current " />
+        <LogoSVG class="h-18 w-auto text-center text-white fill-current" />
       </router-link>
       <!-- Mobile menu button -->
       <div @click="toggleNav" class="flex md:hidden">
-        <button type="button" class="
-              text-red-600
-              hover:text-red-400
-              focus:outline-none focus:text-red-600
-            ">
+        <button
+          type="button"
+          class="text-red-600 hover:text-red-400 focus:outline-none focus:text-red-600"
+        >
           <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-            <path fill-rule="evenodd"
-              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
-            </path>
+            <path
+              fill-rule="evenodd"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+            ></path>
           </svg>
         </button>
       </div>
     </div>
 
     <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-    <ul :class="showMenu ? 'flex' : 'hidden'" class="
-          flex-col
-          mt-8
-          space-y-2
-          bg-white
-          p-2
-          border rounded-lg
-          md:flex md:space-y-0 md:flex-row md:items-center md:space-x-1 md:mt-0
-        ">
-
-      <router-link @click="toggleNav" v-if="storage.getStorageSync('auth_token')" tag="li" active-class="text-red-600 "
-        inactive-class="text-gray-300" class=" hover:text-red-700  px-2 py-2  font-medium" to="/"
-        v-slot="{ href, route, navigate, isExactActive }">
-        {{$t('home')}}
+    <ul
+      :class="showMenu ? 'flex' : 'hidden'"
+      class="flex-col mt-8 space-y-2 bg-white p-2 border rounded-lg md:flex md:space-y-0 md:flex-row md:items-center md:space-x-1 md:mt-0"
+    >
+      <router-link
+        @click="toggleNav"
+        v-if="storage.getStorageSync('auth_token')"
+        tag="li"
+        active-class="text-red-600 "
+        inactive-class="text-gray-300"
+        class="hover:text-red-700 px-2 py-2 font-medium"
+        to="/"
+        v-slot="{ href, route, navigate, isExactActive }"
+      >
+        {{ $t("home") }}
       </router-link>
 
-      <router-link @click="toggleNav" v-if="storage.getStorageSync('auth_token')" tag="li" active-class="text-red-600 "
-        inactive-class="text-gray-300" class=" hover:text-red-700  px-2 py-2  font-medium" to="/profile"
-        v-slot="{ href, route, navigate, isExactActive }">
-        {{$t('profile')}}
+      <router-link
+        @click="toggleNav"
+        v-if="storage.getStorageSync('auth_token')"
+        tag="li"
+        active-class="text-red-600 "
+        inactive-class="text-gray-300"
+        class="hover:text-red-700 px-2 py-2 font-medium"
+        to="/profile"
+        v-slot="{ href, route, navigate, isExactActive }"
+      >
+        {{ $t("profile") }}
       </router-link>
 
-      <router-link @click="toggleNav" v-if="!storage.getStorageSync('auth_token')" tag="li" active-class="text-red-600 "
-        inactive-class="text-gray-300" class=" hover:text-red-700 h px-2 py-2 font-medium" to="/register"
-        v-slot="{ href, route, navigate, isExactActive }">
-        {{$t('register')}}
+      <router-link
+        @click="toggleNav"
+        v-if="!storage.getStorageSync('auth_token')"
+        tag="li"
+        active-class="text-red-600 "
+        inactive-class="text-gray-300"
+        class="hover:text-red-700 h px-2 py-2 font-medium"
+        to="/register"
+        v-slot="{ href, route, navigate, isExactActive }"
+      >
+        {{ $t("register") }}
       </router-link>
 
-      <router-link @click="toggleNav" v-if="!storage.getStorageSync('auth_token')" tag="li" active-class="text-red-600"
-        inactive-class="text-gray-300" class=" hover:text-red-700  h px-2 py-2  font-medium" to="/login"
-        v-slot="{ href, route, navigate, isExactActive }">
-        {{$t('login')}}
+      <router-link
+        @click="toggleNav"
+        v-if="!storage.getStorageSync('auth_token')"
+        tag="li"
+        active-class="text-red-600"
+        inactive-class="text-gray-300"
+        class="hover:text-red-700 h px-2 py-2 font-medium"
+        to="/login"
+        v-slot="{ href, route, navigate, isExactActive }"
+      >
+        {{ $t("login") }}
       </router-link>
 
-      <a v-if="storage.getStorageSync('auth_token')" @click="toggleNav"
-        class="text-gray hover:text-red-700 px-2 py-2  font-medium" @click.prevent="logOut">
-        {{$t('logout')}}
+      <a
+        v-if="storage.getStorageSync('auth_token')"
+        @click="toggleNav"
+        class="text-gray hover:text-red-700 px-2 py-2 font-medium"
+        @click.prevent="logOut"
+      >
+        {{ $t("logout") }}
       </a>
-
     </ul>
   </nav>
 </template>
 
 <!-- https://www.bezkoder.com/vue-3-authentication-jwt/ -->
 <script setup>
-import { ref } from 'vue';
-import LogoSVG from '@/assets/logo.svg';
-import AuthService from '@/services/auth.service'
+import { ref } from "vue";
+import LogoSVG from "@/assets/logo.svg";
+import AuthService from "@/services/auth.service";
 import { useStorage } from "vue3-storage";
 const storage = useStorage();
-
-
 
 let showMenu = ref(false);
 const toggleNav = () => (showMenu.value = !showMenu.value);
 
-
 function logOut() {
   AuthService.logout();
 }
-
-
-
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,120 +1,142 @@
 <template>
-  <div class="border border-solid rounded-lg drop-shadow-lg bg-white  border-white text-black p-2 m-2">
-
+  <div
+    class="border border-solid rounded-lg drop-shadow-lg bg-white border-white text-black p-2 m-2"
+  >
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div class="w-full max-w-md space-y-8">
         <div class="">
-          <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">{{ $t('login') }}</h2>
-          <p class="mt-2 text-center text-sm text-gray-600">
-            Or
-            {{ ' ' }}
-
-            <router-link to="/register" v-slot="{ href, route, navigate, isExactActive }">
-              <a class="font-medium text-red-600 hover:text-red-500" :href="href">{{$t('register')}}</a>
-            </router-link>
-
-          </p>
+          <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-red-600">
+            {{ $t("login") }}
+          </h2>
+          <p class="mt-2 text-center text-sm text-red-600">{{ $t("login_text") }}</p>
         </div>
-        <Form @submit="handleLogin" :validation-schema="schema" class="mt-8 space-y-6" action="#" method="POST">
+        <Form
+          @submit="handleLogin"
+          :validation-schema="schema"
+          class="mt-8 space-y-6"
+          action="#"
+          method="POST"
+        >
           <Field type="hidden" name="remember" value="true" />
 
           <div class="-space-y-px rounded-md shadow-sm">
-            <label for="email-address" class="sr-only">{{ $t('email') }}</label>
-            <Field value="oleg2@goli.com" id="email-address" name="email" type="email" autocomplete="email" required=""
+            <label for="email-address" class="sr-only">{{ $t("email") }}</label>
+            <Field
+              value="oleg2@goli.com"
+              id="email-address"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required=""
               class="relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
-              :placeholder="$t('email')" />
+              :placeholder="$t('email')"
+            />
             <ErrorMessage name="email" class="pt-1 text-red-500 text-xs" />
           </div>
           <div class="-space-y-px rounded-md shadow-sm">
-            <label for="password" class="sr-only">{{ $t('password') }}</label>
-            <Field id="password" value="123123" name="password" type="password" autocomplete="password" required=""
-              class="relative block w-full appearance-none  rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
-              :placeholder="$t('password')" />
+            <label for="password" class="sr-only">{{ $t("password") }}</label>
+            <Field
+              id="password"
+              value="123123"
+              name="password"
+              type="password"
+              autocomplete="password"
+              required=""
+              class="relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
+              :placeholder="$t('password')"
+            />
             <ErrorMessage name="password" class="pt-1 text-red-500 text-xs" />
           </div>
-          <div>
-            <button :disabled="loading" type="submit"
-              class="group relative flex w-full justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
+          <div class="mt-2 text-center text-sm underline">
+            <a href="#"> {{ $t("login_forgot_password") }}</a>
+          </div>
+          <div class="flex justify-center">
+            <button
+              :disabled="loading"
+              type="submit"
+              class="group relative rounded-md border border-transparent bg-red-600 py-2 px-10 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+            >
               <span v-show="loading" class="spinner-border spinner-border-sm"></span>
-              <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-              </span>
-              {{ $t('login') }}
+              <span class="absolute inset-y-0 left-0 flex items-center pl-3"> </span>
+              {{ $t("login_button") }}
             </button>
           </div>
         </Form>
         <div v-if="message" class="alert">
           {{ message }}
         </div>
+
+        <p class="mt-2 text-center text-sm text-red-600">
+          {{ $t("login_signup_text") }}
+          <router-link to="/register" v-slot="{ href, route, navigate, isExactActive }">
+            <a class="font-bold underline hover:text-red-500" :href="href">{{ $t("login_signup") }}</a>
+          </router-link>
+        </p>
+
+        <div class="text-center">
+          <img class="mx-auto rounded" src="@/assets/login/tabitha.png" alt="">
+        </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script setup>
 import { Form, Field, ErrorMessage } from "vee-validate";
 import * as yup from "yup";
-import { ref, computed, onMounted } from 'vue'
-import AuthService from '@/services/auth.service'
-import UserService from '@/services/user.service'
+import { ref, computed, onMounted } from "vue";
+import AuthService from "@/services/auth.service";
+import UserService from "@/services/user.service";
 
 import { useStorage } from "vue3-storage";
-import { storeToRefs } from 'pinia'
-import { useUserStore } from '@/stores/user.store'
-import { useRouter } from 'vue-router';
+import { storeToRefs } from "pinia";
+import { useUserStore } from "@/stores/user.store";
+import { useRouter } from "vue-router";
 import { reject } from "lodash";
 
 const router = useRouter();
-const user = storeToRefs(useUserStore())
+const user = storeToRefs(useUserStore());
 
 const storage = useStorage();
 
 const schema = yup.object().shape({
   email: yup
     .string()
-    .required('email is required!!!') //vue-i18n
-    .email('Email is invalid!'),
-  password: yup.string().required('password is required!!!'),
+    .required("email is required!!!") //vue-i18n
+    .email("Email is invalid!"),
+  password: yup.string().required("password is required!!!"),
 });
 
-const loading = ref(false)
-const message = ref()
-
+const loading = ref(false);
+const message = ref();
 
 onMounted(async () => {
-
-  if (storage.hasKey('auth_token')) {
+  if (storage.hasKey("auth_token")) {
     router.push("/");
   }
-})
+});
 
 async function handleLogin(user) {
   try {
-    console.log('Login.vue (93) handleLogin');
+    console.log("Login.vue (93) handleLogin");
 
-    const token = await AuthService.getToken(user)
-    console.log('Login.vue (94) token', token);
+    const token = await AuthService.getToken(user);
+    console.log("Login.vue (94) token", token);
 
+    const user2 = UserService.get();
 
-    const user2 = UserService.get()
+    console.log("Login.vue (967422) user2", user2);
 
-    console.log('Login.vue (967422) user2', user2);
-
-    router.push("/profile")
-
+    router.push("/profile");
   } catch (error) {
-    console.log('handleLogin error', error)
+    console.log("handleLogin error", error);
 
-    reject(error)
+    reject(error);
   }
-
-
 
   // console.log('torage.hasKey(auth_token)', storage.hasKey('auth_token'))
 
   //router.push("/profile")
   //console.log('profile')
-
 }
-
 </script>
